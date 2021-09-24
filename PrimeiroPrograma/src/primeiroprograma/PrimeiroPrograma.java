104c538bb573b836155ddb8b28970d14e9320b49
Uislan Cruz <uislansk8@hotmail.com>
2021-09-23 22:13:31 -0300

    Ola,Mundo!
    O inicio de tudo! O famoso"Olá, Mundo"

---
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package primeiroprograma;
+
+public class PrimeiroPrograma {
+
+   
+    public static void main(String[] args) {
+       
+
+        System.out.print("Olá, Mundo!");
+        
+    }
+   
+}
+
+
